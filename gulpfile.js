03489879eee52efe0f730d0ba7cacbc8e94da4f6
Matt Larson <matt.a.larson@gmail.com>
2017-09-23 10:59:18 -0500

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const minify = require('gulp-minify-css');
+let gulp = require('gulp');
+let htmlmin = require('gulp-htmlmin');
+let cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify', () => {
+  return gulp.src('public/views/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('public/styles/*.css')
+    .pipe(cleanCSS({debug: true}, function(details) {
+      console.log(details.name + ': ' + details.stats.originalSize);
+      console.log(details.name + ': ' + details.stats.minifiedSize);
+    }))
+  .pipe(gulp.dest('build'));
+});
